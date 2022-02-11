8ac39da142b142a5d50eaf3795a796987d7b9265
Kan Jen Cheng <kanjencheng@berkeley.edu>
2022-02-10 19:41:53 -0800

    final submission

--- hw2/scripts/bored.js
+++ hw2/scripts/bored.js
@@ -11,6 +11,9 @@ async function fetchActivity() {
 }
 
 // END PART 7
+// color loop
+let c = -1;
+let colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple'];
 
 boredButton.addEventListener("click", async () => {
   // empty card
@@ -19,11 +22,18 @@ boredButton.addEventListener("click", async () => {
   // append the text elements as children of the card
   card.replaceChildren();
   const activity = await fetchActivity();
+  card.style.backgroundColor = "#689ef2";
+  c++;
+  if (c == 7) {
+    c = 0;
+  }
   // BEGIN PART 8
   for (const [key, value] of Object.entries(activity)) {
     const detail = document.createElement("p");
     detail.innerHTML = key + ": " + value;
+    detail.style.color = colors[c];
     card.appendChild(detail);
+    c = (c + 1) % 7;
   }
   // END PART 8
 });
