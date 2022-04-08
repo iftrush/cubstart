493171bc3a0ea112d39bc2cf359512a3f2dcfaae
Kan Jen Cheng <kanjencheng@berkeley.edu>
2022-04-07 19:03:26 -0700

    mongodb slow, timeout

---
+++ hw8/node-rest-api/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const userRoute = require("./routes/users");
+const multer = require("multer");
+const path = require("path");
+
+// BEGIN PART 3
+
+const app = express();
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to mongodb");
+  }
+);
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+// "/api/auth"
+app.use("/api/auth", authRoute);
+// "/api/users"
+app.use("/api/users", userRoute);
+// "/api/posts"
+app.use("/api/posts", postRoute);
+
+// END PART 3
+
+// (IGNORE) IMAGE UPLOAD HANDLING
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(8800, () => {
+  console.log("Node server is running!");
+});
---
+++ hw8/node-rest-api/routes/users.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// BEGIN PART 5
+const router = express.Router();
+// END PART 5
+
+// BEGIN PART 8
+
+// UPDATE USER
+router.put("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      // YOUR CODE HERE
+      await User.findByIdAndUpdate(req.body.userId, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    return res.status(403).json("No permissions");
+  }
+});
+
+// GET USER BY ID OR USERNAME
+router.get("/", async (req, res) => {
+  try {
+    // YOUR CODE HERE
+    const userId = req.query.userId;
+    const username = req.query.username;
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    // _doc carries the entire document object in MongoDB. need to remove password and other extranneous information from the response.
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+// GET FOLLOWING/FRIENDS
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    // YOUR CODE HERE
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.following.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendsList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      // Only unpack the properties we need, then push
+      friendsList.push({ _id, username, profilePicture });
+    })
+    res.status(200).json(friendsList);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// FOLLOW USER
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      // YOUR CODE HERE
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.params.id } });
+        res.status(200).json("Followed user");
+      } else {
+        res.status(403).json("Already following");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.status(403).json("Can't follow self");
+  }
+});
+
+// UNFOLLOW USER
+router.put("/:id/unfollow", async (req, res) => {
+  /* YOUR CODE HERE */
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pop: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pop: { following: req.params.id } });
+        res.status(200).json("Unfollowed user");
+      } else {
+        res.status(403).json("You do not follow the user");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.status(403).json("Can't unfollow self");
+  }
+});
+
+// END PART 8
+
+// DELETE USER
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id) {
+    try {
+      await User.findByIdAndDelete(req.body.userId);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    return res.status(403).json("No permissions");
+  }
+});
+
+module.exports = router;
